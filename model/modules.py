aa4e85e15808c065e010f5301f60dcc5279d1c5a
ChairC <974833488@qq.com>
2023-06-23 22:34:54 +0800

    Add: Add modules and network code.

---
+++ model/modules.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+    @Date   : 2023/6/15 17:12
+    @Author : chairc
+    @Site   : https://github.com/chairc
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class EMA:
+    """
+    指数移动平均
+    """
+
+    def __init__(self, beta):
+        """
+
+        :param beta: β
+        """
+        super().__init__()
+        self.beta = beta
+        self.step = 0
+
+    def update_model_average(self, ema_model, current_model):
+        """
+
+        :param ema_model: EMA模型
+        :param current_model: 当前模型
+        :return:
+        """
+        for current_params, ema_params in zip(current_model.parameters(), ema_model.parameters()):
+            old_weight, up_weight = ema_params, current_params.data
+            ema_params.data = self.update_average(old_weight, up_weight)
+
+    def update_average(self, old_weight, new_weight):
+        """
+
+        :param old_weight: 旧权重
+        :param new_weight: 新权重
+        :return:
+        """
+        if old_weight is None:
+            return new_weight
+        return old_weight * self.beta + (1 - self.beta) * new_weight
+
+    def step_ema(self, ema_model, model, step_start_ema=2000):
+        """
+
+        :param ema_model: EMA模型
+        :param model: 原模型
+        :param step_start_ema: 开始 EMA步长
+        :return:
+        """
+        if self.step < step_start_ema:
+            self.reset_parameters(ema_model, model)
+            self.step += 1
+            return
+        self.update_model_average(ema_model, model)
+        self.step += 1
+
+    def reset_parameters(self, ema_model, model):
+        """
+
+        :param ema_model: EMA模型
+        :param model: 原模型
+        :return:
+        """
+        ema_model.load_state_dict(model.state_dict())
+
+
+class SelfAttention(nn.Module):
+    """
+    自注意力模块
+    """
+
+    def __init__(self, channels, size):
+        """
+
+        :param channels: 通道
+        :param size: 尺寸
+        """
+        super(SelfAttention, self).__init__()
+        self.channels = channels
+        self.size = size
+        # pytorch1.8中不支持batch_first，若要支持升级为1.9及以上，或使用一下代码进行转置
+        self.mha = nn.MultiheadAttention(embed_dim=channels, num_heads=4, batch_first=True)
+        self.ln = nn.LayerNorm([channels])
+        self.ff_self = nn.Sequential(
+            nn.LayerNorm([channels]),
+            nn.Linear(in_features=channels, out_features=channels),
+            nn.GELU(),
+            nn.Linear(in_features=channels, out_features=channels),
+        )
+
+    def forward(self, x):
+        """
+
+        :param x: 输入
+        :return:
+        """
+        # 首先进行形状变换，再用swapaxes对新张量的第1和2维度进行交换
+        x = x.view(-1, self.channels, self.size * self.size).swapaxes(1, 2)
+        x_ln = self.ln(x)
+        # pytorch1.8中不支持batch_first，若要支持升级为1.9及以上，或使用一下代码进行转置
+        attention_value, _ = self.mha(x_ln, x_ln, x_ln)
+        attention_value = attention_value + x
+        attention_value = self.ff_self(attention_value) + attention_value
+        return attention_value.swapaxes(2, 1).view(-1, self.channels, self.size, self.size)
+
+
+class DoubleConv(nn.Module):
+    """
+
+    """
+
+    def __init__(self, in_channels, out_channels, mid_channels=None, residual=False):
+        """
+
+        :param in_channels: 输入通道
+        :param out_channels: 输出通道
+        :param mid_channels: 中间通道
+        :param residual: 是否残差
+        """
+        super().__init__()
+        self.residual = residual
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.GroupNorm(num_groups=1, num_channels=mid_channels),
+            nn.GELU(),
+            nn.Conv2d(in_channels=mid_channels, out_channels=out_channels, kernel_size=3, padding=1, bias=False),
+            nn.GroupNorm(num_groups=1, num_channels=out_channels),
+        )
+
+    def forward(self, x):
+        """
+
+        :param x: 输入
+        :return:
+        """
+        if self.residual:
+            return F.gelu(x + self.double_conv(x))
+        else:
+            return self.double_conv(x)
+
+
+class DownBlock(nn.Module):
+    """
+    下采样块
+    """
+
+    def __init__(self, in_channels, out_channels, emb_channels=256):
+        """
+
+        :param in_channels: 输入通道
+        :param out_channels: 输出通道
+        :param emb_channels: 嵌入通道
+        """
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, in_channels, residual=True),
+            DoubleConv(in_channels, out_channels),
+        )
+
+        self.emb_layer = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(
+                emb_channels,
+                out_channels
+            ),
+        )
+
+    def forward(self, x, time):
+        """
+
+        :param x: 输入
+        :param time: 时间
+        :return:
+        """
+        x = self.maxpool_conv(x)
+        emb = self.emb_layer(time)[:, :, None, None].repeat(1, 1, x.shape[-2], x.shape[-1])
+        return x + emb
+
+
+class UpBlock(nn.Module):
+    """
+    上采样块
+    """
+
+    def __init__(self, in_channels, out_channels, emb_channels=256):
+        """
+
+        :param in_channels: 输入通道
+        :param out_channels: 输出通道
+        :param emb_channels: 嵌入通道
+        """
+        super().__init__()
+
+        self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+        self.conv = nn.Sequential(
+            DoubleConv(in_channels=in_channels, out_channels=in_channels, residual=True),
+            DoubleConv(in_channels=in_channels, out_channels=out_channels, mid_channels=in_channels // 2),
+        )
+
+        self.emb_layer = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(
+                emb_channels,
+                out_channels
+            ),
+        )
+
+    def forward(self, x, skip_x, time):
+        """
+
+        :param x: 输入
+        :param skip_x: 需要合并的输入
+        :param time: 时间
+        :return:
+        """
+        x = self.up(x)
+        x = torch.cat([skip_x, x], dim=1)
+        x = self.conv(x)
+        emb = self.emb_layer(time)[:, :, None, None].repeat(1, 1, x.shape[-2], x.shape[-1])
+        return x + emb
---
+++ model/network.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+    @Date   : 2023/6/23 22:26
+    @Author : chairc
+    @Site   : https://github.com/chairc
+"""
+import torch
+import torch.nn as nn
+
+from model.modules import UpBlock, DownBlock, DoubleConv, SelfAttention
+
+
+class UNet(nn.Module):
+    """
+    U型网络
+    """
+
+    def __init__(self, in_channel=3, out_channel=3, channel=None, time_channel=256, num_classes=None, device="cpu"):
+        """
+        初始化UNet网络
+        :param in_channel: 输入通道
+        :param out_channel: 输出通道
+        :param channel: 总通道列表
+        :param time_channel: 时间通道
+        :param num_classes: 类别数
+        :param device: 使用设备
+        """
+        super().__init__()
+        if channel is None:
+            channel = [64, 128, 256, 512]
+        self.device = device
+        self.time_channel = time_channel
+        self.inc = DoubleConv(in_channels=in_channel, out_channels=channel[0])
+        self.down1 = DownBlock(in_channels=channel[0], out_channels=channel[1])
+        self.sa1 = SelfAttention(channels=channel[1], size=32)
+        self.down2 = DownBlock(in_channels=channel[1], out_channels=channel[2])
+        self.sa2 = SelfAttention(channels=channel[2], size=16)
+        self.down3 = DownBlock(in_channels=channel[2], out_channels=channel[2])
+        self.sa3 = SelfAttention(channels=channel[2], size=8)
+
+        self.bot1 = DoubleConv(in_channels=channel[2], out_channels=channel[3])
+        self.bot2 = DoubleConv(in_channels=channel[3], out_channels=channel[3])
+        self.bot3 = DoubleConv(in_channels=channel[3], out_channels=channel[2])
+
+        self.up1 = UpBlock(in_channels=channel[3], out_channels=channel[1])
+        self.sa4 = SelfAttention(channels=channel[1], size=16)
+        self.up2 = UpBlock(in_channels=channel[2], out_channels=channel[0])
+        self.sa5 = SelfAttention(channels=channel[0], size=32)
+        self.up3 = UpBlock(in_channels=channel[1], out_channels=channel[0])
+        self.sa6 = SelfAttention(channels=channel[0], size=64)
+        self.outc = nn.Conv2d(in_channels=channel[0], out_channels=out_channel, kernel_size=1)
+
+        if num_classes is not None:
+            self.label_emb = nn.Embedding(num_classes, time_channel)
+
+    def pos_encoding(self, time, channels):
+        """
+
+        :param time: 时间
+        :param channels: 通道
+        :return:
+        """
+        inv_freq = 1.0 / (10000 ** (torch.arange(0, channels, 2, device=self.device).float() / channels))
+        pos_enc_a = torch.sin(time.repeat(1, channels // 2) * inv_freq)
+        pos_enc_b = torch.cos(time.repeat(1, channels // 2) * inv_freq)
+        pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
+        return pos_enc
+
+    def forward(self, x, time, y=None):
+        """
+
+        :param x: 输入
+        :param time: 时间
+        :param y: 标签
+        :return:
+        """
+        time = time.unsqueeze(-1).type(torch.float)
+        time = self.pos_encoding(time, self.time_channel)
+
+        if y is not None:
+            time += self.label_emb(y)
+
+        x1 = self.inc(x)
+        x2 = self.down1(x1, time)
+        x2 = self.sa1(x2)
+        x3 = self.down2(x2, time)
+        x3 = self.sa2(x3)
+        x4 = self.down3(x3, time)
+        x4 = self.sa3(x4)
+
+        x4 = self.bot1(x4)
+        x4 = self.bot2(x4)
+        x4 = self.bot3(x4)
+
+        output = self.up1(x4, x3, time)
+        output = self.sa4(output)
+        output = self.up2(output, x2, time)
+        output = self.sa5(output)
+        output = self.up3(output, x1, time)
+        output = self.sa6(output)
+        output = self.outc(output)
+        return output
+
+
+if __name__ == "__main__":
+    # 无条件
+    # net = UNet(device="cpu")
+    # 有条件
+    net = UNet(num_classes=10, device="cpu")
+    print(sum([p.numel() for p in net.parameters()]))
+    x = torch.randn(3, 3, 64, 64)
+    t = x.new_tensor([500] * x.shape[0]).long()
+    y = x.new_tensor([1] * x.shape[0]).long()
+    print(net(x, t, y).shape)
