917e2a2a9cc7b1effa4eb494335dd23fff45477e
ChairC <974833488@qq.com>
2023-06-27 16:03:49 +0800

    Add: Add device initializer in 'initializer.py'.

---
+++ utils/initializer.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+    @Date   : 2023/6/20 19:05
+    @Author : chairc
+    @Site   : https://github.com/chairc
+"""
+import torch
+import logging
+import coloredlogs
+
+logger = logging.getLogger(__name__)
+coloredlogs.install(level="INFO")
+
+
+def device_initializer():
+    """
+    该函数在程序第一次运行时初始化运行设备信息
+    :return: cpu或cuda
+    """
+    logger.info("Init program, it is checking the basic setting.")
+    device_dict = {}
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+        device = torch.device("cuda")
+        is_init = torch.cuda.is_initialized()
+        device_count = torch.cuda.device_count()
+        device_name = torch.cuda.get_device_name(device=device)
+        device_cap = torch.cuda.get_device_capability(device=device)
+        device_prop = torch.cuda.get_device_properties(device=device)
+        device_dict["is_init"] = is_init
+        device_dict["device_count"] = device_count
+        device_dict["device_name"] = device_name
+        device_dict["device_cap"] = device_cap
+        device_dict["device_prop"] = device_prop
+        logger.info(device_dict)
+    else:
+        logger.warning("The device is using cpu.")
+        device = torch.device("cpu")
+    return device
