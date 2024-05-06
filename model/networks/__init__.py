3639b6ed7645ab5da135816e2cca219ce371a043
ChairC <974833488@qq.com>
2024-05-06 11:07:59 +0800

    Add: Add initialization operation of networks.

--- model/networks/__init__.py
+++ model/networks/__init__.py
@@ -5,3 +5,11 @@
     @Author : chairc
     @Site   : https://github.com/chairc
 """
+# Diffusion model network
+from .base import BaseNet
+from .cspdarkunet import CSPDarkUnet
+from .unet import UNet
+from .unetv2 import UNetV2
+
+# Super resolution network
+from .sr.srv1 import SRv1
