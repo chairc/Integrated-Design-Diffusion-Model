47f444c087b7c7bfa81a27d45bf790938ba183c9
ChairC <974833488@qq.com>
2024-07-24 21:45:09 +0800

    Add: Add v1.1.4 and v1.1.5 tags.

--- config/version.py
+++ config/version.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 coloredlogs.install(level="INFO")
 
 # IDDM version
-__version__ = ["0.0.1", "1.0.0", "1.0.1", "1.0.2", "1.1.0-stable", "1.1.1", "1.1.2-stable", "1.1.3"]
+__version__ = ["0.0.1", "1.0.0", "1.0.1", "1.0.2", "1.1.0-stable", "1.1.1", "1.1.2-stable", "1.1.3", "1.1.4", "1.1.5"]
 
 
 def get_versions():
