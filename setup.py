358301d8d0326ba8a504d2f906f6bdda85321596
Yu Chen <974833488@qq.com>
2025-03-07 16:35:50 +0800

    fix: Fix install requires not found.
    Signed-off-by: Yu Chen <974833488@qq.com>

--- setup.py
+++ setup.py
@@ -18,28 +18,45 @@ def get_long_description():
     return long_description
 
 
-# Define the metadata of the package
-setup(
-    name="iddm",
-    version="1.1.8",
-    packages=find_packages(),
-    python_requires=">=3.8",
-    install_requires=get_install_requirements(),
-    license="Apache-2.0",
-    description="IDDM: Integrated Design Diffusion Model",
-    long_description=get_long_description(),
-    long_description_content_type="text/markdown",
-    author="chairc",
-    author_email="chenyu1998424@gmail.com",
-    url="https://github.com/chairc/Integrated-Design-Diffusion-Model",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-    ],
-    project_urls={
-        "Documentation": "https://github.com/chairc/Integrated-Design-Diffusion-Model/blob/main/README.md",
-        "Source": "https://github.com/chairc/Integrated-Design-Diffusion-Model",
-        "Tracker": "https://github.com/chairc/Integrated-Design-Diffusion-Model/issues",
-    },
-)
+if __name__ == "__main__":
+    package_list = [
+        "coloredlogs==15.0.1",
+        "gradio==5.0.0",
+        "matplotlib==3.7.1",
+        "numpy==1.25.0",
+        "Pillow==10.3.0",
+        "Requests==2.32.0",
+        "scikit-image==0.22.0",
+        "torch_summary==1.4.5",
+        "tqdm==4.66.3",
+        "pytorch_fid==0.3.0",
+        "fastapi==0.115.6",
+        "tensorboardX==2.6.1",
+        "torch>=1.9.0",
+        "torchvision>=0.10.0"
+    ]
+    # Define the metadata of the package
+    setup(
+        name="iddm",
+        version="1.1.8",
+        packages=find_packages(),
+        python_requires=">=3.8",
+        install_requires=package_list,
+        license="Apache-2.0",
+        description="IDDM: Integrated Design Diffusion Model",
+        long_description=get_long_description(),
+        long_description_content_type="text/markdown",
+        author="chairc",
+        author_email="chenyu1998424@gmail.com",
+        url="https://github.com/chairc/Integrated-Design-Diffusion-Model",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+        ],
+        project_urls={
+            "Documentation": "https://github.com/chairc/Integrated-Design-Diffusion-Model/blob/main/README.md",
+            "Source": "https://github.com/chairc/Integrated-Design-Diffusion-Model",
+            "Tracker": "https://github.com/chairc/Integrated-Design-Diffusion-Model/issues",
+        },
+    )
