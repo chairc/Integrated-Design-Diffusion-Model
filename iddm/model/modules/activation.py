2e9fd3f356208c0964fd1b549e8f386007a3bf7f
ChairC <974833488@qq.com>
2025-09-07 21:52:49 +0800

    chore: Update the get_activation_function.

--- iddm/model/modules/activation.py
+++ iddm/model/modules/activation.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 coloredlogs.install(level="INFO")
 
 
-def get_activation_function(name="silu", inplace=False):
+def get_activation_function(name="silu", inplace=False, **kwargs):
     """
     Get activation function
     :param name: Activation function name
@@ -26,10 +26,15 @@ def get_activation_function(name="silu", inplace=False):
         act = nn.ReLU6(inplace=inplace)
     elif name == "silu":
         act = nn.SiLU(inplace=inplace)
-    elif name == "lrelu":
-        act = nn.LeakyReLU(0.1, inplace=inplace)
+    elif name == "leakyrelu":
+        negative_slope = kwargs.get("negative_slope", 0.2)
+        act = nn.LeakyReLU(negative_slope=negative_slope, inplace=inplace)
     elif name == "gelu":
         act = nn.GELU()
+    elif name == "prelu":
+        act = nn.PReLU()
+    elif name == "tanh":
+        act = nn.Tanh()
     else:
         logger.warning(msg=f"Unsupported activation function type: {name}")
         act = nn.SiLU(inplace=inplace)
