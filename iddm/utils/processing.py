0c9d29dc395fa3056d7fa292585d1182944774ac
cheny <chairc1998@163.com>
2025-09-15 11:33:57 +0800

    feat: Add image convert method.

--- iddm/utils/processing.py
+++ iddm/utils/processing.py
@@ -9,6 +9,9 @@ import io
 import base64
 import logging
 import coloredlogs
+import numpy as np
+
+from torchvision import transforms
 
 from PIL import Image
 
@@ -45,3 +48,59 @@ def image_to_base64(image=None, image_name=None, encoding="utf-8"):
     image.save(image_buffer, format=image_format)
     image_base64 = base64.b64encode(s=image_buffer.getvalue()).decode(encoding=encoding)
     return image_base64
+
+
+def pil_image_to_tensor(image):
+    """
+    Convert a PIL image to a PyTorch tensor.
+    :param image: PIL Image
+    :return: PyTorch tensor
+    """
+    if not isinstance(image, Image.Image):
+        logger.error("Image must be PIL Image")
+        raise TypeError("Input must be a PIL image.")
+    # Convert the PIL image to a PyTorch tensor
+    image_tensor = transforms.ToTensor()(image)
+    return image_tensor
+
+
+def numpy_image_to_tensor(image):
+    """
+    Convert a NumPy image to a PyTorch tensor.
+    :param image: NumPy array
+    :return: PyTorch tensor
+    """
+    if not isinstance(image, (np.ndarray,)):
+        logger.error("Image must be NumPy array")
+        raise TypeError("Input must be a NumPy array.")
+    # Convert the NumPy array to a PyTorch tensor
+    image_tensor = transforms.ToTensor()(Image.fromarray(image))
+    return image_tensor
+
+
+def tensor_to_pil_image(tensor):
+    """
+    Convert a PyTorch tensor to a PIL image.
+    :param tensor: PyTorch tensor
+    :return: PIL Image
+    """
+    if not isinstance(tensor, (np.ndarray,)):
+        logger.error("Input must be a NumPy array")
+        raise TypeError("Input must be a NumPy array.")
+    # Convert the PyTorch tensor to a PIL image
+    image = transforms.ToPILImage()(tensor)
+    return image
+
+
+def tensor_to_numpy_image(tensor):
+    """
+    Convert a PyTorch tensor to a NumPy image.
+    :param tensor: PyTorch tensor
+    :return: NumPy array
+    """
+    if not isinstance(tensor, (np.ndarray,)):
+        logger.error("Input must be a NumPy array")
+        raise TypeError("Input must be a NumPy array.")
+    # Convert the PyTorch tensor to a NumPy array
+    image = np.array(transforms.ToPILImage()(tensor))
+    return image
